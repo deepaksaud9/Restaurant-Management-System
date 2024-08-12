182154c1b9bfae6e74e972c9872136d9afc1ad49
Deepak <77203703+deepaksaud9@users.noreply.github.com>
2024-08-12 19:09:23 +0545

    feat: add post category in admin-service

--- restaurant_angular/src/app/modules/admin/admin-components/add-category/add-category.component.ts
+++ restaurant_angular/src/app/modules/admin/admin-components/add-category/add-category.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AdminService } from '../../admin-service/admin.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { read } from 'fs';
+import { NzMessageService } from 'ng-zorro-antd/message';
 
 @Component({
   selector: 'app-add-category',
@@ -10,10 +12,13 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class AddCategoryComponent implements OnInit {
 
   categoryForm: FormGroup;
+  selectedFile: File | null;
+  imagePreview: string | ArrayBuffer| null;
 
   constructor(
     private service: AdminService,
     private fb: FormBuilder,
+    private message: NzMessageService,
   ) { }
 
   ngOnInit(): void {
@@ -24,7 +29,35 @@ export class AddCategoryComponent implements OnInit {
   }
 
   postCategory(){
-    console.log(this.categoryForm.value)
+    console.log(this.categoryForm.value);
+    const formdata: FormData = new FormData();
+    formdata.append("img",this.selectedFile);
+    formdata.append("name",this.categoryForm.get("name").value);
+    formdata.append("description",this.categoryForm.get("description").value);
+    this.service.postCategory(formdata).subscribe(
+      (res) => {
+        console.log(res);
+        if(res.id != null){
+          this.message.success("category posted successfully", {nzDuration:5000});
+        }else if(res.id == null){
+            this.message.error("Something went wrong",{nzDuration:5000})
+        }
+      } 
+    );
+
+  }
+
+  onFileSelected(event: any){
+    this.selectedFile = event.target.Files[0];
+    this.previewImage() 
+  }
+
+  previewImage(){
+    const reader = new FileReader();
+    reader.onload = () =>{
+      this.imagePreview = reader.result;
+    }
+    reader.readAsDataURL(this.selectedFile);
   }
 
 }
